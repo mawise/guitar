363a80c538412b858569719d5b3d81c605c81032
Matthew Wise <matthew.rs.wise@gmail.com>
2013-12-27 18:17:38 -0500

    added scrolling to read.rb

--- read.rb
+++ read.rb
@@ -4,6 +4,7 @@ rows = ARGV[2] || 36
 cols = ARGV[3] || 150
 splits = ARGV[1] || 2
 splits = splits.to_i
+offset = 0
 
 filename = ARGV[0]
 puts "Filename: #{filename}"
@@ -18,27 +19,34 @@ file.each_line {|line| lines << line}
 file.close
 
 rows = lines.size if lines.size < rows
-
-(0...rows).each do |line_number|
-  display_line =  ""
-  (0...splits).each do |split|
-    if line_number + (rows*split) < lines.size
-      display_line << '|' unless split == 0
-      begin
-        display_line << lines[line_number+(rows*split)][0..(cols/splits)][0...-1] #drop the trailing /n
-      rescue Exception => e
-        puts "line_number: #{line_number}"
-        puts "lines.size: #{lines.size}"
-        puts "rows: #{rows}"
-        puts "splits: #{splits}"
-        puts "split: #{split}"
-        puts "display line: #{display_line}."
-        puts "lines[linenumber+rows*split]: #{lines[line_number+(rows*split)]}"
-        raise e
+loop do
+  (0...rows).each do |line_number|
+    display_line =  ""
+    (0...splits).each do |split|
+      if line_number + (rows*split) + offset < lines.size
+        display_line << '|' unless split == 0
+        begin
+          display_line << lines[line_number+(rows*split)+offset][0..(cols/splits)][0...-1] #drop the trailing /n
+        rescue Exception => e
+          puts "line_number: #{line_number}"
+          puts "lines.size: #{lines.size}"
+          puts "rows: #{rows}"
+          puts "splits: #{splits}"
+          puts "split: #{split}"
+          puts "display line: #{display_line}."
+          puts "lines[linenumber+rows*split]: #{lines[line_number+(rows*split)]}"
+          raise e
+        end
+        extra = (split+1)*(cols/splits) - display_line.size
+        extra.times {display_line<<" "}
       end
-      extra = (split+1)*(cols/splits) - display_line.size
-      extra.times {display_line<<" "}
     end
+    puts display_line
   end
-  puts display_line
-end
+  input = STDIN.gets.chomp
+  offset +=rows if input == ''
+  offset -=rows if [' ', 'b'].include? input[0]
+  offset = 0 if offset < 0
+  break unless [nil, ' ', 'b'].include? input[0]
+  break if offset > lines.size
+end 
